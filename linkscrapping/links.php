ec35132b156efb63b451b02556e13c50c200b0bd
eduardomorais <edsousa@gmail.com>
2010-10-31 00:37:01 +0100

    Mais elegante, mais docs.

--- linkscrapping/links.php
+++ linkscrapping/links.php
@@ -6,63 +6,74 @@
 /** Gera Links para PDFs da Assembleia
  *
  */
-
+ 
+ 
+// Define PDFs:
+// caminho -> base do nome do ficheiro -> num. de PDFs (por sessao legislativa)
 $sacar = array(
-	'DAR-I-' => array (
-		"http://arnet/sites/XILEG/DARI/DARIArquivo/2ª Sessão Legislativa" => 30,
-		"http://arnet/sites/XILEG/DARI/DARIArquivo/1ª Sessão Legislativa" => 84,
-	),
-	'DARI' => array(
-		"http://arnet/sites/XLEG/DARI/DARIArquivo/4ª Sessão Legislativa" => 106,
-		"http://arnet/sites/XLEG/DARI/DARIArquivo/3ª Sessão Legislativa" => 111,
-		"http://arnet/sites/XLEG/DARI/DARIArquivo/2ª Sessão Legislativa" => 110,
-		"http://arnet/sites/XLEG/DARI/DARIArquivo/1ª Sessão Legislativa" => 149,
-		
-		"http://arnet/sites/IXLEG/DARI/DARIArquivo/3.ª Sessão Legislativa" => 24,
-		"http://arnet/sites/IXLEG/DARI/DARIArquivo/2.ª Sessão Legislativa" => 108,
-		"http://arnet/sites/IXLEG/DARI/DARIArquivo/1.ª Sessão Legislativa" => 146,
-		
-		"http://arnet/sites/VIIILEG/DARI/DARIArquivo/3.ª Sessão Legislativa" => 33,
-		"http://arnet/sites/VIIILEG/DARI/DARIArquivo/2.ª Sessão Legislativa" => 107,
-		"http://arnet/sites/VIIILEG/DARI/DARIArquivo/1.ª Sessão Legislativa" => 91,
-	),
-	'DAR' => array(		
-		"http://arnet/sites/VIILEG/DARI/DARIArquivo/4.ª Sessão Legislativa" => 108,
-		"http://arnet/sites/VIILEG/DARI/DARIArquivo/3.ª Sessão Legislativa" => 89,
-		"http://arnet/sites/VIILEG/DARI/DARIArquivo/2.ª Sessão Legislativa" => 112,
-		"http://arnet/sites/VIILEG/DARI/DARIArquivo/1.ª Sessão Legislativa" => 109,
-	)
+	"http://arnet/sites/VIILEG/DARI/DARIArquivo/%d.ª Sessão Legislativa" =>
+		array('DAR' => 
+			array(109, 112, 89, 108)),
+	"http://arnet/sites/VIIILEG/DARI/DARIArquivo/%d.ª Sessão Legislativa" =>
+		array('DARI' 
+			=> array(91, 107, 33)),
+	"http://arnet/sites/IXLEG/DARI/DARIArquivo/%d.ª Sessão Legislativa" =>
+		array('DARI' 
+			=> array(146, 108, 24)),
+	"http://arnet/sites/XLEG/DARI/DARIArquivo/%dª Sessão Legislativa" =>
+		array('DARI' 
+			=> array(149, 110, 111, 106)),
+	"http://arnet/sites/XILEG/DARI/DARIArquivo/%dª Sessão Legislativa" =>
+		array('DAR-I-' 
+			=> array(84, 30)) 
+		// o ultimo numero diz respeito à actual legislatura, logo em actualização
 );
 
-foreach ($sacar as $formato => $localPaths) {
-	foreach ($localPaths as $localPath => $num) {
-		echo "<h3>$localPath</h3><p>";
 
-		for ($i=1;$i<=$num;$i++) {
-			$ni = str_pad($i, 3, "0", STR_PAD_LEFT);
+// Percorrer o array
+foreach ($sacar as $caminho => $ficheiros) {
+	foreach ($ficheiros as $template => $quantos) {
+		$s = 0;
+		foreach ($quantos as $num) {
+			$s++;
+			$path = sprintf($caminho, $s);
+			
+			echo "<p>$path</p><code>"; // html?
 
-			$nome = $formato.$ni.'.pdf';
-			$query = http_build_query(array(
-					'doc'	=> strToHex(base64_encode($localPath."/$nome")),
-					'nome'	=> $nome
-				), '', '&');
+			for ($i=1;$i<=$num;$i++) {
+				$ni = str_pad($i, 3, "0", STR_PAD_LEFT);
+
+				$nome = $template.$ni.'.pdf';
+				$query = http_build_query(array(
+						'doc'	=> strToHex(base64_encode($path."/$nome")),
+						'nome'	=> $nome
+					), '', '&');
+					
+				$url = 'http://app.parlamento.pt/darpages/dardoc.aspx?'.$query;
 				
-			$url = 'http://app.parlamento.pt/darpages/dardoc.aspx?'.$query;
+				//------------------------------------------------------------------
+				// em vez de fazer links podemos fazer qualquer coisa com estas URLs
+				//
+				echo '<a href="'.$url.'">'.$ni.'</a>  ';
+				//------------------------------------------------------------------
 
-			echo '<a href="'.$url.'">'.$nome.'</a> | ';
-
+			}
+			
+			echo "</code>"; // html?
+			
 		}
-
-		echo "</p>";
 	}
 }
-// FUNCTIONS
 
-function strToHex($string)
-{
+
+
+//-----------------------------------------
+// FUNCTIONS
+//-----------------------------------------
+
+function strToHex($string) {
     $hex='';
-    for ($i=0; $i < strlen($string); $i++)
-    {
+    for ($i=0; $i < strlen($string); $i++)     {
         $hex .= dechex(ord($string[$i]));
     }
     return $hex;
