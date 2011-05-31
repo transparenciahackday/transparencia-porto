499ca9cda16d394e10cc6f1f9cc60885a305a90a
Pedro Rodrigues <medecau@gmail.com>
2011-05-31 19:52:23 +0100

    output to json cache pages with shelve

--- tools/depscrap.py
+++ tools/depscrap.py
@@ -1,16 +1,31 @@
 # -*- coding: utf-8 -*-
 import urllib
 from BeautifulSoup import BeautifulSoup
-from BeautifulSoup import BeautifulStoneSoup
-import csv
+#from BeautifulSoup import BeautifulStoneSoup
+from datetime import datetime as dt
+from json import dumps
 import os
 import shelve
 import sys
+from hashlib import sha1
+
+def hash(str):
+    hash=sha1()
+    hash.update(str)
+    return hash.hexdigest()
 
 def getpage(url):
-    return urllib.urlopen(url).read()
+    try:
+        page= s[hash(url)]
+    except:
+        page= urllib.urlopen(url).read()
+        s[hash(url)]=page
+        s.sync()
+    return page
 
 s=shelve.open('cache.shelve')
+
+DATASETS='../../datasets/'
 
 URL_DEPS_ACTIVOS='http://www.parlamento.pt/DeputadoGP/Paginas/DeputadosemFuncoes.aspx'
 FORMATTER_URL_BIO_DEP='http://www.parlamento.pt/DeputadoGP/Paginas/Biografia.aspx?BID=%d'
@@ -37,18 +52,27 @@ for dep in deps:
     dep_bid=int(depurl[depurl.find('BID=')+4:])
     if dep_bid>max:
         max=dep_bid
-max*=1.05
+max=int(max*1.05)
 print 'Testing up to %d' % max
 
-#s['bios']={}
-#for i in range(max):
-for i in range(100,110):
-    soup=BeautifulSoup(getpage(FORMATTER_URL_BIO_DEP % i))
-    print soup.find('span',dict(id= 'ctl00_ctl13_g_8035397e_bdf3_4dc3_b9fb_8732bb699c12_ctl00_ucNome_rptContent_ctl01_lblText')).text
-    print '##########'
+deprows=[]
 
-#s.sync()
-#s.close()
+for i in range(0, max):
+    print i
+    #for i in range(100,103):
+    soup=BeautifulSoup(getpage(FORMATTER_URL_BIO_DEP % i))
+    name=soup.find('span',dict(id= 'ctl00_ctl13_g_8035397e_bdf3_4dc3_b9fb_8732bb699c12_ctl00_ucNome_rptContent_ctl01_lblText'))
+    if name:
+        deprows.append({'id': i,
+                        'name': name.text,
+                        'date': dt.utcnow().isoformat()})
+
+depsfp=open(DATASETS+'deputados.json', 'w')
+depsfp.write(dumps(deprows))
+depsfp.close()
+
+s.sync()
+s.close()
 
 
 
