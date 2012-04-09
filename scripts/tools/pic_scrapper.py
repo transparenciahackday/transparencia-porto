809462208e86d738b0ac89a35384ae5d78d2cd51
Ricardo Lafuente <r@sollec.org>
2012-04-09 21:53:42 +0100

    Picscrapper: corrigido para usar o input JSON

--- scripts/tools/pic_scrapper.py
+++ scripts/tools/pic_scrapper.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python
 
-
+import sys, os
 from urllib import urlretrieve
-from csv import reader
-from csv import writer
+import json
+#from csv import reader
+#from csv import writer
 #from time import sleep
 
-
+mp_file = '/home/rlafuente/proj/thd/datasets/deputados.json'
 pic_url_formatter='http://app.parlamento.pt/webutils/getimage.aspx?id=%s&type=deputado'
 dest='imgs/'
 
 def main():
-    csv_MP=reader(open('MP.csv'),delimiter='|', quotechar='"')
-    
-    for mp in csv_MP:
-        mp_id=mp[1]
-        urlretrieve(pic_url_formatter % mp_id, '%s%s.jpg' % (dest,mp_id))
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+        print "Directory 'imgs/' created."
+
+    # csv_MP=reader(open('MP.csv'),delimiter='|', quotechar='"')
+    mp_json = json.loads(open(mp_file, 'r').read())
+    for mp_id in mp_json:
         print 'retrieving picture with id: %s' % mp_id
+        urlretrieve(pic_url_formatter % mp_id, '%s%s.jpg' % (dest,mp_id))
     
     print "it's almost done"
     print 'do find ./imgs/ -size -722c -exec rm {} \;'
     print 'to clean up things'
+
 if __name__ == '__main__':
     main()
 
